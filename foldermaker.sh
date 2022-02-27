bc814641ea10113ceed8374763ccee44ea9edc9c
tyrsme <98252671+tyrsme@users.noreply.github.com>
2022-02-27 18:13:02 +0000

    Week2 updates
    Week2 updates

--- foldermaker.sh
+++ foldermaker.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 
-#!/bin/bash 
-      
 read -p "type the name of the folder you would like to create" folderName 
+#asking user input for the newly created folderName
       
-mkdir "$folderName" 
+mkdir "$folderName"
+#make new directory
      
--- passwordCheck.sh
+++ passwordCheck.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-hashCheck=secret.txt
+hashCheck=$(cat secret.txt)
+#linking the hash of the password created in setPasswords.sh and setting as variable hashCheck
 
 read -sp "type a secret password: " userPassword
+#prompts user for input and doesn't print. Saves to var userPassword
 
-echo -s "$userPassword" | sha256sum > userHash.txt
+userHash=$( echo "$userPassword" | sha256sum )
+#setting the output of the users sectret password (after being run through sha256sum hash algorithm) as variable userPassword
 
-userHash=userHash.txt
-
-if cmp -s -- "$hashCheck" "$userHash"; then
-    echo "Access Granted" $?
+if [ "$hashCheck" == "$userHash" ]; then
+    echo "Access Granted"
+    exit 0
+#if the hashes of each secret password match, grant access with error code 0
 else
     echo "Access Denied" $?
+    exit 1
+#if the hashes of each secret password don't match, do NOT grant access with error code 1
 fi
 
